964c024abe4edbf9365017ca2e70f92a26e78e82
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-12-10 21:54:38 +0000

    hamburger nav styled and finished, navbar finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,15 +10,15 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-      <h1 className="w-full text-4xl font-bold hero-font text-[#57321c]">
+      <h1 className="w-full text-4xl font-bold hero-font text-[#a04008]">
         BBR.
       </h1>
       <ul className="hidden md:flex text-white text-xl">
         <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
+        <li className="p-4">Photos</li>
+        <li className="p-4">Riders</li>
+        <li className="p-4">Locations</li>
+        <li className="p-4">Competition</li>
       </ul>
       <div onClick={handleNav} className="block md:hidden">
         {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
@@ -26,18 +26,18 @@ const Navbar = () => {
       <ul
         className={
           nav
-            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-yellow-500 ease-in-out duration-500"
             : "ease-in-out duration-500 fixed left-[-100%]"
         }
       >
-        <h1 className="w-full text-3xl hero-font font-bold text-[#00df9a] m-4">
+        <h1 className="w-full text-3xl hero-font font-bold text-[#a04008] m-4">
           BBR.
         </h1>
-        <li className="p-4 border-b border-gray-600">Home</li>
-        <li className="p-4 border-b border-gray-600">Company</li>
-        <li className="p-4 border-b border-gray-600">Resources</li>
-        <li className="p-4 border-b border-gray-600">About</li>
-        <li className="p-4">Contact</li>
+        <li className="p-4 border-b border-black text-black">Home</li>
+        <li className="p-4 border-b border-black text-black">Photos</li>
+        <li className="p-4 border-b border-black text-black">Riders</li>
+        <li className="p-4 border-b border-black text-black">Locations</li>
+        <li className="p-4 text-black">Competition</li>
       </ul>
     </div>
   );
